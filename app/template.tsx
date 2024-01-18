9d0b7cef70dec227aa5255c1e3b1fa0c1ea1e37a
Mialiokhin <a.mialiokhin@gmail.com>
2024-01-18 18:51:38 +0300

    fix[sc-0002]: added contacts

--- app/template.tsx
+++ app/template.tsx
@@ -4,14 +4,23 @@ import React from "react"
 
 import { Container } from "@mui/material"
 
+import ContactBlock from "@/app/_components/ContactBlock/ContactBlock"
 import FiltersBar from "@/app/_components/FiltersBar/FiltersBar"
 import useGetNasaApod from "@/app/_hooks/useGetNasaApod"
+
+const contacts = [
+  { type: "telegram", value: "melleshka" },
+  { type: "github", value: "Mialiokhin" },
+  { type: "email", value: "a.mialiokhin@gmail.com" },
+  { type: "phone", value: "+375297177209" },
+]
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const { isLoading } = useGetNasaApod()
 
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <ContactBlock contacts={contacts} />
       <Container>
         <FiltersBar loading={isLoading || false} />
       </Container>
